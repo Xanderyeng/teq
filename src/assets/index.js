20c7bb7b41a8f1518b2781a3b7c043e0586e55e9
Xanderyeng <50344667+Xanderyeng@users.noreply.github.com>
2023-04-04 11:58:59 +0300

    small typo mistakes -dataScience

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,7 @@
 export { default as logo } from "./Logo.svg";
 export { default as toggle } from './dark_light.svg'
 export { default as dataAnalytics } from "./data-analytics-icon-1.svg"
-export { default as datatScience } from './data-science-icon-1.svg'
+export { default as dataScience } from './data-science-icon-1.svg'
 export { default as dataEngineering } from './data-engineering.svg'
 export { default as cyberSecurity } from './cyber-security.svg'
 
